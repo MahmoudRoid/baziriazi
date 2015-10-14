09bbaa43f8fb1a0c83bcacb78e68aa7ab1988ef1
Mahmoud <m.mahmoudreza1@yahoo.com>
2015-10-14 22:05:45 +0330

    double back to exit

--- app/src/main/java/mahmoud/baziriazi/MainActivity.java
+++ app/src/main/java/mahmoud/baziriazi/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    boolean doubleBackToExitPressedOnce=false;
     ImageView imgplay;
     ImageView imgexit;
     final CharSequence[] levels = {"ساده","متوسط","سخت"};
@@ -128,17 +130,21 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
-////        super.onBackPressed();
-//        Intent myintent=new Intent(getApplicationContext(),MainActivity.class);
-//        myintent.putExtra("m","m");
-//        myintent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        startActivityForResult(myintent,Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
 
